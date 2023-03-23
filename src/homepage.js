84dbe02d033ceeda18faaa2a99535480b1b960d6
harrysonhall <114112064+harrysonhall@users.noreply.github.com>
2023-03-23 11:00:53 -0400

    refactored my CSS for the Global Header and Footer and overrall page layout globally to be much cleaner by using CSS Media Queries. Its now fully responsive in a pleasing fashion. Verified this on my Mac's Chrome Browser, on the iPad's Safari  on my iPhone Safari browser

--- src/homepage.js
+++ src/homepage.js
@@ -1,74 +1,74 @@
-import './home.css';
-import './uncollapsed.css';
-import './collapsed.css';
+import './css/home-page.css';
+import './css/header.css';
+import './css/footer.css';
 
-const log = console.log
+// const log = console.log
 
-const header_logo 				= document.querySelector('#header-logo');
-const header_logo_image 		= document.querySelector('#header-logo-image');
-const header_logo_text 			= document.querySelector('#header-logo-text');
-const header_hr 				= document.querySelector('#header-hr');
-const header_navigation 		= document.querySelector('#header-navigation');
-const header_navigation_menu 	= document.querySelector('#header-navigation-menu');
+// const header_logo 				= document.querySelector('#header-logo');
+// const header_logo_image 		= document.querySelector('#header-logo-image');
+// const header_logo_text 			= document.querySelector('#header-logo-text');
+// const header_hr 				= document.querySelector('#header-hr');
+// const header_navigation 		= document.querySelector('#header-navigation');
+// const header_navigation_menu 	= document.querySelector('#header-navigation-menu');
 
-addEventListener("DOMContentLoaded", (event) => {
-	log('loaded')
-	log(window.innerHeight, window.innerWidth)
+// addEventListener("DOMContentLoaded", (event) => {
+// 	log('loaded')
+// 	log(window.innerHeight, window.innerWidth)
 
 
-	if(window.innerWidth <= 910) {
+// 	if(window.innerWidth <= 910) {
 	
-		console.log(window.innerWidth);
+// 		console.log(window.innerWidth);
 
-		collapsed();
-	} else {
+// 		collapsed();
+// 	} else {
 
-		uncollapsed();
-	}
-})
-
-
-
-window.addEventListener('resize', (e) => {
-
-	if(window.innerWidth <= 1110) {
-		console.log(window.innerWidth)
-	}
+// 		uncollapsed();
+// 	}
+// })
 
 
-	if(window.innerWidth <= 910) {
+
+// window.addEventListener('resize', (e) => {
+
+// 	if(window.innerWidth <= 1110) {
+// 		console.log(window.innerWidth)
+// 	}
+
+
+// 	if(window.innerWidth <= 910) {
 	
-		log('resize', window.innerWidth);
+// 		log('resize', window.innerWidth);
 
-		collapsed();
-	}
+// 		collapsed();
+// 	}
 
-	else {
+// 	else {
 
-		uncollapsed();
-	}
+// 		uncollapsed();
+// 	}
 	
-})
+// })
 
-		function collapsed() {
-			header_logo.classList.value = "header-logo-collapsed";
-			header_logo_image.classList.value = "header-logo-image-collapsed";
-			header_logo_text.classList.value = "header-logo-text-collapsed";
-			header_hr.classList.value = "header-hr-collapsed";
-			header_navigation.classList.value = "header-naviagation-collapsed";
-			header_navigation_menu.classList.value = "header-navigation-menu-collapsed";
-			if(window.innerWidth <= 420){
-				log('under 420')
-				header_logo_image.classList.add('header-logo-image-collapsed-420px-and-under');
-				header_navigation_menu.classList.add('header-navigation-menu-collapsed-420px-and-under');
-			}
-		}
+// 		function collapsed() {
+// 			header_logo.classList.value = "header-logo-collapsed";
+// 			header_logo_image.classList.value = "header-logo-image-collapsed";
+// 			header_logo_text.classList.value = "header-logo-text-collapsed";
+// 			header_hr.classList.value = "header-hr-collapsed";
+// 			header_navigation.classList.value = "header-naviagation-collapsed";
+// 			header_navigation_menu.classList.value = "header-navigation-menu-collapsed";
+// 			if(window.innerWidth <= 420){
+// 				log('under 420')
+// 				header_logo_image.classList.add('header-logo-image-collapsed-420px-and-under');
+// 				header_navigation_menu.classList.add('header-navigation-menu-collapsed-420px-and-under');
+// 			}
+// 		}
 
-		function uncollapsed(){
-			header_logo.classList.value = "header-logo-uncollapsed";
-			header_logo_image.classList.value = "header-logo-image-uncollapsed";
-			header_logo_text.classList.value = "header-logo-text-uncollapsed";
-			header_hr.classList.value = "header-hr-uncollapsed";
-			header_navigation.classList.value = "header-naviagation-uncollapsed";
-			header_navigation_menu.classList.value = "header-navigation-menu-uncollapsed";
-		}
+// 		function uncollapsed(){
+// 			header_logo.classList.value = "header-logo-uncollapsed";
+// 			header_logo_image.classList.value = "header-logo-image-uncollapsed";
+// 			header_logo_text.classList.value = "header-logo-text-uncollapsed";
+// 			header_hr.classList.value = "header-hr-uncollapsed";
+// 			header_navigation.classList.value = "header-naviagation-uncollapsed";
+// 			header_navigation_menu.classList.value = "header-navigation-menu-uncollapsed";
+// 		}
